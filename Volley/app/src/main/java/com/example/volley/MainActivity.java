3552cbcffb6cdbad1aa3577155a75bdedbacf86e
Alan <szxtjnu@gmail.com>
2022-07-28 21:58:27 +0800

    refresh-over

--- Volley/app/src/main/java/com/example/volley/MainActivity.java
+++ Volley/app/src/main/java/com/example/volley/MainActivity.java
@@ -26,15 +26,20 @@ import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "hello";
-    String url = "https://pic.imgdb.cn/api/avatar";
+    String url1 = "https://pic.imgdb.cn/api/avatar";
+    String url2 = "https://pic.imgdb.cn/api/avatar";
     private ImageView imageView;
+    private SwipeRefreshLayout swipeRefreshLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SwipeRefreshLayout swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
         imageView = findViewById(R.id.imageView);
         RequestQueue queue = Volley.newRequestQueue(this);
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
@@ -44,18 +49,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    void loadImage(){
+
+    void loadImage() {
+        Random random = new Random();
+        String url = random.nextBoolean() ? url1 : url2;
         Glide.with(this)
                 .load(url)
                 .placeholder(R.drawable.ic_launcher_background)
                 .listener(new RequestListener<Drawable>() {
                     @Override
                     public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        if (swipeRefreshLayout.isRefreshing()) {
+                            swipeRefreshLayout.setRefreshing(false);
+                        }
                         return false;
                     }
 
                     @Override
                     public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        if (swipeRefreshLayout.isRefreshing()) {
+                            swipeRefreshLayout.setRefreshing(false);
+                        }
                         return false;
                     }
                 })
