a845b1c8359ecbca34eb88ae801d84f8b63a7965
Alan <szxtjnu@gmail.com>
2022-07-28 14:40:30 +0800

    url-ok

--- Volley/app/src/main/java/com/example/volley/MainActivity.java
+++ Volley/app/src/main/java/com/example/volley/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.volley;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "hello";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView textView = findViewById(R.id.textView);
+        String url = "https://szxtnu.cn";
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(
+                StringRequest.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        textView.setText(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e(TAG, "onErrorResponse: ", error);
+                    }
+                }
+        );
+        queue.add(stringRequest);
     }
 }
